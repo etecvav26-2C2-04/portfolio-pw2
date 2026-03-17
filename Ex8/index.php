def0571c9fab586e038e3a2307b0417428468575
Victor-camargo742 <pvipcptor@gmail.com>
2026-03-17 19:18:17 -0300

    Add files via upload
    adicionado exercício 8

---
+++ Ex8/index.php
@@ -0,0 +1,30 @@
+<!--
+Exercício 8 - Soma de 0 até N com Função
+Faça um programa que receba um número inteiro, 
+determinando a soma de 0 até tal número (incluso) por meio de uma
+função soma(n) que retorne tal soma.
+-->
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>somafunc</title>
+</head>
+<body>
+
+     <h1>Soma de 0 ate um numero</h1>
+
+
+    <br>
+    <form name="frmIntervalo" action="ex8.php" method="POST">
+
+        <label for="txtnum">Informe um numero:</label>
+        <input type="number" name="txtnum"/>
+        <input type="submit" name="btnCalcular" value="somar"/>
+        
+    </form>
+</body>
+</html>
