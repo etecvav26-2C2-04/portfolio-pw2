83b37a8fb0ab8d530c8a442621cfa26ee0aeafc3
Victor-camargo742 <pvipcptor@gmail.com>
2026-05-20 16:29:24 -0300

    Create cadastro.php
    estou fazendo o cadastro.php

---
+++ 2-bim/farmacia-vav/cadastro.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'config/conexao.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $nome = $_POST['nome'];
+    $fabricante = $_POST['fabricante'];
+    $preco = $_POST['preco'];
+    $estoque = $_POST['estoque'];
+
+    $sql = $pdo->prepare("
+        INSERT INTO produtos
+        (nome, fabricante, preco, estoque)
+        VALUES
+        (:nome, :fabricante, :preco, :estoque)
+    ");
